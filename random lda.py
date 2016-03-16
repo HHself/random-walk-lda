cb2670d58ab96c2b1412770bbb78f5429421cd5d
HH <hh87know@gmail.com>
2016-03-16 16:08:38 +0800

    Signed-off-by: HH <hh87know@gmail.com>

--- random lda.py
+++ random lda.py
@@ -7,10 +7,12 @@ vocab = lda.datasets.load_reuters_vocab()
 titles = lda.datasets.load_reuters_titles()
 model = lda.LDA(n_topics=20, n_iter=10, random_state=1)
 model.fit(X)
-print model.nzw_
+# print model.nzw_
 
-'''
+
 topic_word = model.topic_word_  # model.components_ also works
+print topic_word
+'''
 n_top_words = 8
 for i, topic_dist in enumerate(topic_word):
     topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
