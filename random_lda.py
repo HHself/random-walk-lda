4832eb89d8575998de5d9075c5a62d4a501a8578
HH <hh87know@gmail.com>
2016-03-16 16:06:19 +0800

    Signed-off-by: HH <hh87know@gmail.com>

--- random_lda.py
+++ random_lda.py
@@ -7,7 +7,6 @@ vocab = lda.datasets.load_reuters_vocab()
 titles = lda.datasets.load_reuters_titles()
 model = lda.LDA(n_topics=20, n_iter=10, random_state=1)
 model.fit(X)
-print model.nzw_
 
 '''
 topic_word = model.topic_word_  # model.components_ also works
